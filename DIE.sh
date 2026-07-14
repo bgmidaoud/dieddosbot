845164eb709fff113217d160919208c18c64ba38
bgmidaoud <bgmidaoud@gmail.com>
2026-07-14 18:21:26 +0530

    Update DIE.sh

--- DIE.sh
+++ DIE.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/Bot/DIE.py",
+    "postStartCommand": "python3 /workspaces/Bot/bot.py",
     "customizations": {
         "vscode": {
             "settings": {
